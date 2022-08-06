548b79fe60316279f021d710d8c7c967a0e3a97f
Array_fish <uekusa_hideaki@yahoo.co.jp>
2022-08-06 23:53:40 +0900

    [remove] reductant files [add] add .nojekyell to adapt _nuxt

--- docs/public/_nuxt/about-da848e2f.mjs
+++
@@ -1,1 +0,0 @@
-import{a as t,o as s,b as o,F as a,e,f as n}from"./entry-cb24f976.mjs";const r={},c=e(" Array fish"),_=n("br",null,null,-1),l=e(" Vue.js/Nuxt/node.js/Python/Flask/C++ ");function d(u,f){return s(),o(a,null,[c,_,l],64)}var h=t(r,[["render",d]]);export{h as default};
